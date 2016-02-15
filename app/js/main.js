ac28f372cc683ba09c4767acc240d4527510dce4
Abhishek Jain <abhishek.jain2692@gmail.com>
2016-02-15 21:31:39 +0530

    Initial commit to add all files.

---
+++ app/js/main.js
@@ -0,0 +1,23 @@
+'use strict';
+// TODO
+
+if ('serviceWorker' in navigator) {
+    console.log('Service Worker is supported');
+    navigator.serviceWorker.register('/sw.js').then(function () {
+        return navigator.serviceWorker.ready;
+    }).then(function (reg) {
+        console.log('Service Worker is ready :^)', reg);
+        reg.pushManager.subscribe({
+           userVisibleOnly: true 
+        })
+        .then(function(sub) {
+           console.log('endpoint:', sub.endpoint);
+           document.getElementById('endpoint').textContent = sub.endpoint; 
+        });
+    }).catch(function (error) {
+        document.getElementById('endpoint').textContent = error;
+        console.log('Service Worker error :^(', error);
+    });
+} else {
+    alert('error');   
+}
